bf5c9abdbd3389078c85c778dade58f037779fec
sabreys <trinityforce37@gmail.com>
2022-06-11 16:11:41 +0300

    Initial commit

---
+++ Object3D.py
@@ -0,0 +1,56 @@
+import numpy as np
+from matrix_functions import *
+import pygame as pg
+
+from numba import njit
+
+
+@njit(fastmath=True)
+def any_func(arr, a, b):
+    return np.any((arr == a) | (arr == b)) #fps improvement
+
+
+class Object3D:
+    def __init__(self, render, vertexes, faces):
+        self.render = render
+        self.vertexes = np.array([np.array(v) for v in vertexes])
+
+        self.faces = np.array([np.array(face) for face in faces])
+        self.draw_vertexes = False
+
+    def draw(self):
+        self.screen_projection()
+        self.rotate_Y(0.1)
+        self.rotate_Z(0.1)
+
+    def screen_projection(self):
+        vertexes = self.vertexes @ self.render.camera.camera_matrix()
+        vertexes = vertexes @ self.render.projection.project_matrix
+        vertexes /= vertexes[:, -1].reshape(-1, 1)
+        vertexes[(vertexes > 2) | (vertexes < -2)] = 0  # higher is better for getting near to objects
+        vertexes = vertexes @ self.render.projection.to_screen_matrix
+        vertexes = vertexes[:, :2]
+
+        for face in self.faces:
+            polygon = vertexes[face]
+            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
+                pg.draw.polygon(self.render.screen, pg.Color('orange'), polygon, 1)
+
+        # for vertex in vertexes:
+        #     if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+        #         pg.draw.circle(self.render.screen, pg.Color("white"), vertex, 2)
+
+    def translate(self, pos):
+        self.vertexes = self.vertexes @ translate(pos)
+
+    def scale(self, n):
+        self.vertexes = self.vertexes @ scale(n)
+
+    def rotate_X(self, angle):
+        self.vertexes = self.vertexes @ rotate_x(angle)
+
+    def rotate_Y(self, angle):
+        self.vertexes = self.vertexes @ rotate_y(angle)
+
+    def rotate_Z(self, angle):
+        self.vertexes = self.vertexes @ rotate_z(angle)
---
+++ main.py
@@ -0,0 +1,65 @@
+# Matrix_mark01
+
+# Sic mundus creatus est.
+# Hinc erunt adaptationes mirabiles, quarum modus est hic.
+
+import pygame as pg
+from camera import *
+from projection import *
+from Object3D import *
+
+
+class Renderer:
+
+    def __init__(self):
+        pg.init()
+        self.RES = self.WIDTH, self.HEIGHT = 1100, 900
+        self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
+        self.FPS = 60
+        self.screen = pg.display.set_mode(self.RES)
+        self.clock = pg.time.Clock()
+        self.create_obj()
+        self.object.rotate_X(180)
+
+    def create_obj(self):
+        self.camera= Camera(self,[-0.65646274,2.00236745,-3.4492734])
+        self.projection = Projection(self)
+        self.object =  self.get_object_from_file("my_potato_face.obj")
+
+    def get_object_from_file(self,filepath):
+        vertex,faces = [],[]
+        with open(filepath) as f:
+            for line in f:
+                if line.startswith('v '):
+                    vertex.append([float(i) for i in line.split()[1:]]+[1])
+                elif line.startswith('f'):
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split('/')[0])-1 for face_ in faces_])
+        return Object3D(self,vertex,faces)
+
+
+    def draw(self):
+       self.screen.fill(pg.Color("black"))
+       self.object.draw()
+
+
+    def run(self):
+
+        light = pg.image.load('circle.png')
+
+        while True:
+            self.draw()
+            self.camera.control()
+
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.set_caption(str(self.clock.get_fps()))  # Show fps on top bar
+            pg.display.flip()
+            self.clock.tick(self.FPS)
+            print(self.camera.pos)
+
+
+
+
+if __name__ == '__main__':
+    matrix = Renderer()
+    matrix.run()
